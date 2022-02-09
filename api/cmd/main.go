8f39096014474af2f3cbb43b9d3e879c608abab0
jordanengstrom <jordan.engstrom@gmail.com>
2022-02-08 18:03:28 -0700

    successfully retrieving Author data from API

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -15,18 +15,21 @@ func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/authors", h.GetAuthors).Methods(http.MethodGet)
-	router.HandleFunc("/authors", h.CreateAuthor).Methods(http.MethodPost)
-	router.HandleFunc("/authors/{id}", h.GetAuthorById).Methods(http.MethodGet)
-	router.HandleFunc("/authors/{id}", h.UpdateAuthor).Methods(http.MethodPut)
-	router.HandleFunc("/authors/{id}", h.DeleteAuthor).Methods(http.MethodDelete)
+	subrouter := router.PathPrefix("/api").Subrouter()
 
-	router.HandleFunc("/books", h.GetBooks).Methods(http.MethodGet)
-	router.HandleFunc("/books", h.CreateBook).Methods(http.MethodPost)
-	router.HandleFunc("/books/{id}", h.GetBookById).Methods(http.MethodGet)
-	router.HandleFunc("/books/{id}", h.UpdateBook).Methods(http.MethodPut)
-	router.HandleFunc("/books/{id}", h.DeleteBook).Methods(http.MethodDelete)
+	subrouter.HandleFunc("/authors", h.GetAuthors).Methods(http.MethodGet)
+	subrouter.HandleFunc("/authors", h.CreateAuthor).Methods(http.MethodPost)
+	subrouter.HandleFunc("/authors/{id}", h.GetAuthorById).Methods(http.MethodGet)
+	subrouter.HandleFunc("/authors/{id}", h.UpdateAuthor).Methods(http.MethodPut)
+	subrouter.HandleFunc("/authors/{id}", h.DeleteAuthor).Methods(http.MethodDelete)
 
-	log.Println("API is running!")
-	http.ListenAndServe(":8080", router)
+	subrouter.HandleFunc("/books", h.GetBooks).Methods(http.MethodGet)
+	subrouter.HandleFunc("/books", h.CreateBook).Methods(http.MethodPost)
+	subrouter.HandleFunc("/books/{id}", h.GetBookById).Methods(http.MethodGet)
+	subrouter.HandleFunc("/books/{id}", h.UpdateBook).Methods(http.MethodPut)
+	subrouter.HandleFunc("/books/{id}", h.DeleteBook).Methods(http.MethodDelete)
+
+	port := ":8080"
+	log.Printf("API is running on http://localhost%v\n", port)
+	http.ListenAndServe(port, router)
 }
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface IAuthor {
+  firstName: string,
+  middleName: string,
+  lastName: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ui';
+  public authors: IAuthor[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  // ngOnInit() {
+  //   this.getAuthors();
+  // }
+
+  ngOnInit() {
+    this.httpClient.get<IAuthor[]>('/api/authors')
+                        .subscribe({
+                          next: data => { 
+                            this.authors = data;
+                          },
+                          error: error => {
+                            console.log('There was an error!', error);
+                          }
+                        })
+  }
 }
+
+// {
+//   'firstName': 'Benny',
+//   'middleName': 'Warchild',
+//   'lastName': 'Engstrom'
+// },
+// {
+//   'firstName': 'Goose',
+//   'middleName': 'Warchild',
+//   'lastName': 'Engstrom'
+// }
